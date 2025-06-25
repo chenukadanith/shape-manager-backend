7bf5097ac73e5d566b8a7e27617761df23ec4497
chenuka <chenukadanith@gmai.com>
2025-06-25 17:30:05 +0530

    configure test file

---
+++ src/test/java/com/example/shapemanegement/ShapeManegementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shapemanegement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapeManegementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
