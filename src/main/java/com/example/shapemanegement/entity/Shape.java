17484c18e1bcf803c7b46dbff8aea80d9dd8dbea
chenuka <chenukadanith@gmai.com>
2025-06-25 17:15:19 +0530

    create shape entity

---
+++ src/main/java/com/example/shapemanegement/entity/Shape.java
@@ -0,0 +1,38 @@
+package com.example.shapemanegement.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Table(name = "shapes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Shape {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+
+    @Column(name = "coordinates", columnDefinition = "JSON")
+    private String coordinates;
+
+    // For circles
+    @Column(name = "center_x")
+    private Double centerX;
+
+    @Column(name = "center_y")
+    private Double centerY;
+
+    @Column(name = "radius")
+    private Double radius;
+}
