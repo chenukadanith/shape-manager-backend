794ec7e25a28f067af69bcaf0860a5c14346346c
chenuka <chenukadanith@gmai.com>
2025-06-25 17:28:42 +0530

    main file

---
+++ src/main/java/com/example/shapemanegement/ShapeManegementApplication.java
@@ -0,0 +1,13 @@
+package com.example.shapemanegement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShapeManegementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShapeManegementApplication.class, args);
+	}
+
+}
